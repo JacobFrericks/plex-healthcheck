b6929a98f356573afab5811b4b12585ae5e8ae38
Jacob Frericks <jacob.frericks@gmail.com>
2019-10-19 09:46:01 -0500

    Adding try catch

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ PASSWORD = os.environ.get('PLEX_PASSWORD')
 SERVERNAME = os.environ.get('PLEX_SERVER_NAME')
 
 account = MyPlexAccount(USER, PASSWORD)
-plex = account.resource(SERVERNAME).connect()
 
-movies = plex.library.section('Movies')
-if not movies.search(unwatched=True):
-    # Plex is not working...
+try:
+    plex = account.resource(SERVERNAME).connect()
+    movies = plex.library.section('Movies')
+    print("Plex is up!")
+except:
     print("RESTARTING PLEX...")
     os.system("taskkill /f /im  Plex Media Server.exe")
     os.system('"C:\\Program Files (x86)\\Plex\\Plex Media Server\\Plex Media Server.exe"')
-else:
-    print("Plex is up!")
